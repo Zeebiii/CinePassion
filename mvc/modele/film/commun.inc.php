790d309c856dffdb082fe1a2b31d7a852bb4f543
Zeebiii <37512801+Zeebiii@users.noreply.github.com>
2019-10-03 16:22:56 +0200

    Premier commit.
    Premier commit pour alimenter le depot.

---
+++ mvc/modele/film/commun.inc.php
@@ -0,0 +1,19 @@
+<?php
+/*======= C O N T R O L E U R ====================================================================================
+	fichier				: ./mvc/modele/fil/commun.inc.php
+	auteur				: Mathias MASSELON
+	date de création	: Septembre 2019
+	date de modification:
+	rôle				: le modele commun a chaque page du site
+  ================================================================================================================*/
+
+/**
+ * Classe relative au modele commun a toutes les pages du site
+ * @author Mathias MASSELON
+ * @version 1.0
+ * @copyright Mathias Masselon - Septembre 2019
+ */
+class modeleFilm extends modele{
+    
+}
+	
